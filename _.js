6b05cc83313f36b3596a2d600c6bb63204af45cc
Aaron-Acheampong <aaron.acheampong@amalitech.org>
2021-12-04 17:13:57 +0000

    first commit

---
+++ _.js
@@ -0,0 +1,107 @@
+
+const _ = {
+     clamp(number, lower, upper) {
+      if (number < lower) { return lower; }
+      else if (number > upper) { return upper; }
+      else { return number; }
+    },
+
+    inRange(number, start, end) {
+        if (end === undefined) { 
+          end = start;
+          start = 0;
+        }
+    
+        if (start > end) {
+          let temp = start;
+          start = end;
+          end = temp;
+    
+        }
+    
+        if (number < start) { return false; }
+        else if (number >= end) { return false; }
+        else { return true; }
+      },
+
+      words(string) {
+        words = string.split(' ');
+        return words;
+      },
+
+      pad(string, length) {
+        if (length <= string.length) {
+          return string;
+        } 
+        
+          const startPaddingLength = Math.floor((length - string.length) / 2);
+          const endPaddingLength = length - string.length - startPaddingLength;
+          const paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
+          
+          return paddedString;
+      
+      
+        
+      },
+
+      has(object, key) {
+        const hasValue = object[key]
+        if (hasValue != undefined){
+          return true;
+        } else {return false;}
+      },
+
+      invert(object) {
+        let invertedObject = {};
+        for (let key in object) {
+          const originalValue = object[key];
+          invertedObject = {originalValue : key};
+        }
+        return invertedObject;
+      },
+
+      findKey(object, predicate) {
+        for(let key in object){
+          let value = object[key];
+          let predicateReturnValue = predicate(value);
+          if(predicateReturnValue){
+            return key;
+          }
+        }
+        return undefined;
+      },
+
+      drop(array, n) {
+        if(n === undefined){ n = 1;}
+        let droppedArray = array.slice(n, array.length);
+        return droppedArray;
+      },
+
+      dropWhile(array, predicate) {
+        let dropNumber = array.findIndex(function(element, index) {return !predicate(element, index, array)});
+        let droppedArray = this.drop(array, dropNumber);
+        return droppedArray;
+      },
+
+      chunk(array, size) {
+        if(size === undefined){size = 1;}
+        let arrayChunks =[];
+        for(let i = 0; i < array.length; i+=size){
+          let arrayChunk = array.slice(i, i+size);
+          arrayChunks.push(arrayChunk);
+        }
+        return arrayChunks;
+      }
+      
+
+
+  };
+  
+  
+  
+
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
